a9960307f719b65bc6cefca1508eddcb8a08d883
CoderWanFeng <875143930@qq.com>
2023-09-18 00:58:20 +0800

    publish 0.1.0

--- poocr/api/ocr2excel.py
+++ poocr/api/ocr2excel.py
@@ -3,22 +3,21 @@
 @作者 ：B站/抖音/微博/小红书/公众号，都叫：程序员晚枫
 @微信 ：CoderWanFeng : https://mp.weixin.qq.com/s/yFcocJbfS9Hs375NhE8Gbw
 @个人网站 ：www.python-office.com
-@Date    ：2023/3/25 18:53 
+@Date    ：2023/3/25 18:53
 @Description     ：
 '''
 import json
 from pathlib import Path
 
 import pandas as pd
-from pofile import get_files, mkdir
-from poprogress import simple_progress
-
 import poocr
+from pofile import get_files, mkdir
 from poocr.api.ocr import VatInvoiceOCR
+from poprogress import simple_progress
 
 
 def VatInvoiceOCR2Excel(input_path, output_path=None, output_excel='VatInvoiceOCR2Excel.xlsx', img_url=None,
-                        configPath=None):
+                        configPath=None, id=None, key=None):
     """
     批量识别发票，并保存在Excel中
     :param input_path: 发票存放位置，可以填单个文件，也可以填一个目录
@@ -43,8 +42,7 @@ def VatInvoiceOCR2Excel(input_path, output_path=None, output_excel='VatInvoiceOC
     res_df = []  # 装全部识别的结果
     for vat_img in simple_progress(vat_img_files):
         try:
-            api_res = VatInvoiceOCR(img_path=str(vat_img), img_url=img_url, configPath=configPath)
-
+            api_res = VatInvoiceOCR(img_path=str(vat_img), img_url=img_url, configPath=configPath, id=id, key=key)
             api_res_json = json.loads(str(api_res))
             VatInvoiceInfos = api_res_json['VatInvoiceInfos']
             dict_pandas = {}  # 存放一行数据
@@ -59,7 +57,7 @@ def VatInvoiceOCR2Excel(input_path, output_path=None, output_excel='VatInvoiceOC
         except:
             continue
     # 整理全部识别结果
-    if len(res_df)>0:
+    if len(res_df) > 0:
         res_excel = res_df[0]
         for index, line_df in enumerate(res_df):
             if index == 0:
@@ -68,6 +66,7 @@ def VatInvoiceOCR2Excel(input_path, output_path=None, output_excel='VatInvoiceOC
         pd.DataFrame(res_excel).to_excel(str(abs_output_excel))  # 写入Excel
     else:
         print(f'该文件夹下，没有任何符合条件的发票图片')
+
 
 def TrainTicketOCR2Excel(input_path: str, output_excel: str = r'./TrainTicketOCR2Excel.xlsx', img_url: str = None,
                          configPath: str = None) -> None:
