0c9ddbe3c54ef90326cbddd197d5366de62a6f8f
CoderWanFeng <875143930@qq.com>
2023-11-30 00:04:13 +0800

    增加：识别车牌，保存到Excel里

--- dev/20231129/car2excel.py
+++ dev/20231129/car2excel.py
@@ -16,16 +16,16 @@ import poocr
 SecretId = os.getenv("SecretId")
 SecretKey = os.getenv("SecretKey")
 # SecretKey = os.getenv("TEMP")
-print(SecretId, SecretKey)
+# print(SecretId, SecretKey)
 
 # 读取所有环境变量
 # all_env_vars = os.environ
 # print(all_env_vars)
-test_json = poocr.ocr.LicensePlateOCR(img_path=r'./files/car_test.png', id=SecretId, key=SecretKey)
+# test_json = poocr.ocr.LicensePlateOCR(img_path=r'./files/car_test.png', id=SecretId, key=SecretKey)
+# #
+# # # {"CardNo": "6225768888888888", "BankInfo": "招商银行(03080000)", "ValidDate": "07/2023", "CardType": "贷记卡", "CardName": "招商银行信用卡", "BorderCutImage": null, "CardNoImage": null, "WarningCode": null, "QualityValue": null, "RequestId": "dfa4eb5c-e84a-4204-a23f-d71d92bff551"}
+# df = pd.DataFrame(json.loads(str(test_json)), index=[0])
 #
-# # {"CardNo": "6225768888888888", "BankInfo": "招商银行(03080000)", "ValidDate": "07/2023", "CardType": "贷记卡", "CardName": "招商银行信用卡", "BorderCutImage": null, "CardNoImage": null, "WarningCode": null, "QualityValue": null, "RequestId": "dfa4eb5c-e84a-4204-a23f-d71d92bff551"}
-df = pd.DataFrame(json.loads(str(test_json)), index=[0])
+# df.to_excel('car.xlsx', index=False)
 
-df.to_excel('car.xlsx', index=False)
-
-poocr.ocr2excel.BankCardOCR2Excel(input_path=r'./files/img.png', id=SecretId, key=SecretKey)
+poocr.ocr2excel.LicensePlateOCR2Excel(input_path=r'./files/car_test.png', id=SecretId, key=SecretKey)
