34526776a6e7630c08b7d74136c2ddb9929e1ae2
CoderWanFeng <875143930@qq.com>
2023-11-30 22:09:56 +0800

    更新：官网教程

--- poocr/__init__.py
+++ poocr/__init__.py
@@ -5,7 +5,7 @@ from poocr.lib.Const import SPLIT_LINE
 print(SPLIT_LINE)
 print('【poocr库】，功能持续更新中')
 print('使用有问题 or 提交你的功能需求 or 参与项目开发')
-print('1、全部功能【视频 & 文字】教程：https://mp.weixin.qq.com/s/WxICBZZSgkm-OrvXB82hbg')
+print('1、全部功能【视频 & 文字】教程：https://www.python-office.com/course-002/2-poocr/2-poocr.html')
 print('2、请+【项目交流群】：http://t.cn/A65MiFvH')
 print('3、本开源项目的【代码仓库】：https://github.com/CoderWanFeng/poocr')
 print('4、给小白的【50讲Python自动化办公】：https://www.python-office.com/course/50-python-office.html')
